fce740f1c5817dcfe3bbcce2f70ee2ebc3e5d26b
Kamlesh Bhatt <kb55262555@gmail.com>
2024-02-29 11:44:38 +0530

    feat: Added sigin, signup and logout controllers

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
-
-const app = express();
-
+import connectToMongoDB from "./db/connectToMongoDB.js";
 dotenv.config();
 
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  // root route
-  res.send("Server is ready");
-});
+
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// app.get("/", (req, res) => {
+//   // root route
+//   res.send("Server is ready");
+// });
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
+});
