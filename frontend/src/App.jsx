83dea4959771510830c3ce866c8d486c5a5f9c66
Kamlesh Bhatt <kb55262555@gmail.com>
2024-03-01 12:02:56 +0530

    feat: Signup page UI done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/Signup";
 
 function App() {
-  return <div className='p-4 h-screen flex items-center justify-center'>
-    <Login />
-  </div>;
+  return (
+    <div className='p-4 h-screen flex items-center justify-center'>
+      {/* <Login /> */}
+      <SignUp />
+    </div>
+  );
 }
 
 export default App;
