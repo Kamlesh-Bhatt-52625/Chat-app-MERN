bb1418040674664c0bcee2009b8c073ddc27bbd2
Kamlesh Bhatt <kb55262555@gmail.com>
2024-03-01 12:51:20 +0530

    feat: User searchbar and coversation bar done

---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchInput from "./SearchInput";
+import Conversations from "./Conversations";
+
+const Sidebar = () => {
+  return <div className="">
+        <SearchInput />
+        <div className="divider px-3"></div>
+        <Conversations />
+        {/* <LogoutButton /> */}
+  </div>;
+};
+
+export default Sidebar;
