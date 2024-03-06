862f33de28d3f5568fded5d8241d359ee6d1f639
Kamlesh Bhatt <kb55262555@gmail.com>
2024-03-06 10:33:21 +0530

    feat: Sent and recived time added to the UI

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,12 +1,12 @@
-import React from "react";
 import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
 import useConversation from "../../zustand/useConversation";
 
 const Message = ({ message }) => {
   const { authUser } = useAuthContext();
   const { selectedConversation } = useConversation();
   const fromMe = message.senderId === authUser._id;
-
+  const formattedTime = extractTime(message.createdAt);
   const chatClassName = fromMe ? "chat-end" : "chat-start";
   const profilePic = fromMe
     ? authUser.profilePic
@@ -21,11 +21,11 @@ const Message = ({ message }) => {
         </div>
       </div>
 
-      <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+      <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>
         {message.message}
       </div>
       <div className={`chat-footer opacity-50 text-xs flex gap-1 items-center`}>
-        12:42
+        {formattedTime}
       </div>
     </div>
   );
